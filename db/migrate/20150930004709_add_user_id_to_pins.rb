06dec454a64a3c70dc7ec211ed941772ecbad5d6
Dinh Le <dinh.v.le@gmail.com>
2015-10-05 14:35:54 -0500

    Fixed AddUserIdToPins

--- db/migrate/20150930004709_add_user_id_to_pins.rb
+++ db/migrate/20150930004709_add_user_id_to_pins.rb
@@ -1,4 +1,4 @@
-rakclass AddUserIdToPins < ActiveRecord::Migration
+class AddUserIdToPins < ActiveRecord::Migration
   def change
     add_column :pins, :user_id, :integer
     add_index :pins, :user_id
